7406c973522cbe0375db06d0f0cf3896d08503ad
Red Fox <foxilator@live.com>
2018-03-16 14:33:56 -0400

    using wcf service now

--- ViewModels/CurrentWeather.cs
+++ ViewModels/CurrentWeather.cs
@@ -8,22 +8,18 @@ using Windows.Data.Xml.Dom;
 using XMLDocument = Windows.Data.Xml.Dom.XmlDocument;
 using System.ComponentModel;
 using Prism.Mvvm;
+using WeatherApp.ServiceReference1;
 
 namespace WeatherApp.ViewModels
 {
     class CurrentWeather : BindableBase
     {
         private string currentLocation;
-        private WebRequest request;
+        private string request;
         private Weather immediateWeather = new Weather();
-        private static readonly string URL_START = "https://api.apixu.com/v1/current.xml";
-        private static readonly string URL_KEY = "ff436f1b91a7474aab4223122170710";
-        private static readonly string URL = URL_START + "?q={0}&key=" + URL_KEY;
-        private string url;
 
         public CurrentWeather(string location)
         {
-            
             this.CurrentLocation = location;
             Load();
         }
@@ -46,70 +42,41 @@ namespace WeatherApp.ViewModels
 
         protected void Load()
         {
-            url = URL;
-            //request = WebRequest.Create(string.Format(url, this.currentLocation));
             immediateWeather.PropertyChanged += ImmediateWeather_PropertyChanged;
         }
 
-        protected void SetRequest(string val)
-        {
-            request = WebRequest.Create(string.Format(url, val));
-        }
-
-        //public void GetWeather()
-        //{
-            
-        //    request.BeginGetResponse(new AsyncCallback(SetWeather), null);
-        //}
-
         public void GetWeather(string zip)
         {
-            SetRequest(zip);
-            Task<WebResponse> response = request.GetResponseAsync();
-            response.Wait();
-            SetWeather(response.Result);
+            request = zip;
+            GetWeather();
         }
 
         public void GetWeather(double lat, double lon)
         {
-            SetRequest(lat.ToString() + "," + lon.ToString());
-            Task<WebResponse> response = request.GetResponseAsync();
-            response.Wait();
-            SetWeather(response.Result);
+            request = lat.ToString() + "," + lon.ToString();
+            GetWeather();
         }
 
         public void GetWeather(IPAddress ip)
         {
-            SetRequest(ip.ToString());
-            Task<WebResponse> response = request.GetResponseAsync();
-            response.Wait();
-            SetWeather(response.Result);
+            request = ip.ToString();
+            GetWeather();
         }
 
-        private string Parse(string s, string name)
+        private void GetWeather()
         {
-            XMLDocument doc = new XMLDocument();
-            doc.LoadXml(s);
-            IXmlNode node = doc.SelectSingleNode(name);
-            return node.InnerText;
+            Task<ImmediateWeather> task = new Service1Client().GetDataAsync(request);
+            task.Wait();
+            SetWeather(task.Result);
         }
 
-        public void SetWeather(IAsyncResult result)
+        private void SetWeather(ImmediateWeather weather)
         {
-            SetWeather(request.EndGetResponse(result));
-        }
-
-        public void SetWeather(WebResponse response)
-        {
-            Stream s = response.GetResponseStream();
-            StreamReader sr = new StreamReader(s);
-            string r = sr.ReadToEnd();
-
-            immediateWeather.Temperature = decimal.Parse(Parse(r, "/root/current/temp_f"));
-            immediateWeather.WindSpeed = decimal.Parse(Parse(r, "/root/current/wind_mph"));
-            immediateWeather.WindDirection = Parse(r, "/root/current/wind_dir");
-            immediateWeather.Clouds = Parse(r, "/root/current/condition/text");
-            CurrentLocation = Parse(r, "/root/location/name") + ", " + Parse(r, "/root/location/region") + ", " + Parse(r, "/root/location/country");
+            this.Clouds = weather.clouds;
+            this.Temperature = weather.temp;
+            this.WindDirection = weather.windDir;
+            this.WindSpeed = weather.windSpeed;
+            this.CurrentLocation = weather.city + ", " + weather.state + ", " + weather.country;
         }
 
         public string CurrentLocation
